61679cc7b2da75937631dcff0c0ee924e3d6c010
Fuad-Abdela <fuadabdela2072@yahoo.com>
2022-09-09 06:06:36 -0700

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,41 +1,28 @@
 #include <stdio.h>
 
 /**
- *
- * *main - Display memory size of variables
- *
- * *
- *
- * *Return: Always 0 (Success)
- *
- * */
+*
+* main - Display memory size of variables
+*
+* Return: 0
+*/
 
 int main(void)
 
 {
 
-		char ch;
+	char ch;
 
-			int in;
+	int in;
+	long lng;
+	long long lolng;
+	float flt;
 
-				long lng;
-
-					long long lolng;
-
-						float flt;
-
-
-
-							printf("Size of a char: %lu byte(s)\n", sizeof(ch));
-
-								printf("Size of an int: %lu byte(s)\n", sizeof(in));
-
-									printf("Size of a long int: %lu byte(s)\n", sizeof(lng));
-
-										printf("Size of a long long int: %lu byte(s)\n", sizeof(lolng));
-
-											printf("Size of a float: %lu byte(s)\n", sizeof(flt));
-
-												return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(ch));
+	printf("Size of an int: %ld byte(s)\n", sizeof(in));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(lng));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lolng));
+	printf("Size of a float: %ld byte(s)\n", sizeof(flt));
+											return (0);
 
 }
