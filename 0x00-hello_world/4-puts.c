f468578015e629647f936e2ad4cbfd2212c359ca
Fuad-Abdela <fuadabdela2072@yahoo.com>
2022-09-09 02:14:30 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include <string.h>
+
+
 
 /**
  *
- *  *main - How to use Puts function
+ *  * main -displays the phrase "Programming is
  *
- *    *Return: Always 0 (Success)
- *    **/
-int main(void)
-{
-		char phrase[] = "\"Programming is like building a multilingual puzzle";
+ *   *,like building a multilingual puzzle
+ *
+ *    * Return: 0 if exited properly, non-zero otherwise
+ *
+ *     */
 
-			puts(phrase);
-				return (0);
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
