22a01c40787667d0a46aceaa6deb412e91a51ff5
Fuad-Abdela <fuadabdela2072@yahoo.com>
2022-09-22 00:51:37 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,21 +6,17 @@
  * @s2: 2nd string
  * Return: 0 if matching, and ns1 - ns2 if not matching
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i, n;
+	int i;
 
-	i = 0;
-
-	n = 0;
-
-	while (n == 0)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		n = *(s1 + i) - *(s2 + i);
-		i++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
+
 	return (0);
 }
