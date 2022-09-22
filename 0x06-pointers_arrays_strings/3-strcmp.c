b3552c0855690008a9ac039ae01652cfae6bb8be
Fuad-Abdela <fuadabdela2072@yahoo.com>
2022-09-22 00:46:40 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, n;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	i = 0;
+
+	n = 0;
+
+	while (n == 0)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		n = *(s1 + i) - *(s2 + i);
+		i++;
 	}
-	
 	return (0);
 }
